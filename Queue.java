abf654627a02cc4a9415f5b0f13d78a751de3670
Manu Mohan <64430181+macbmc@users.noreply.github.com>
2021-02-09 23:08:21 +0530

    Update Queue.java

--- Queue.java
+++ Queue.java
@@ -1,28 +1,22 @@
 import java.util.Scanner;
-
 public class Queue {
 
-    static Scanner sc = new Scanner(System.in);
-      static int  n, front = -1, rear = -1, item;
-      static int[] Ar = new int[10];
-      static int p = 1;
+	 Scanner sc = new Scanner(System.in);
+	int  n, front = -1, rear = -1, item;
+	 int[] Ar = new int[10];
 	
 	Queue() {
 		
 	}
 	
 	Queue(int n){
-		int ch;
+		int choice;
 		do
 		  {
-			System.out.println("1.Enqueue(to insert elements)");
-			System.out.println("2.Dequeue(to remove elements)");
-			System.out.println("3. Display(to view existing entered elements)");
-			System.out.println("4.Exit");
-			
-			System.out.println("Enter your choice:");
-			ch=sc.nextInt();
-		    switch (ch)
+			System.out.printf("\n1.Enqueue\n2.Dequeue\n3.Exit\n");
+			System.out.printf("Enter the choice: ");
+			choice=sc.nextInt();
+		    switch (choice)
 		    {
 		    case 1:
 		      enqueue(n);
@@ -30,34 +24,28 @@ public class Queue {
 		    case 2:
 		      dequeue();
 		      break;
-		    case 3:
-		      display(rear);
-		      break;
-		      
 		    default:
 		      break;
 		    }
-		  } while (ch < 4);
+		  } while (choice != 3);
 	}
 		
 	public static void main(String[] args) {
 		Scanner sc1 = new Scanner(System.in);
 		int n;
 		
-		
-		System.out.println("Enter the size of Queue:");
+		System.out.printf("Enter the size of Queue:");
 		n=sc1.nextInt();
 		
 		new Queue(n);
 		  
 	}
 	
-	
-	public static void enqueue(int n)
+	public  void enqueue(int n)
 	{
 	  if (rear >= n - 1)
 	  {
-	    System.out.println("Queue is overflow");
+	    System.out.println("Overflow");
 	  }
 	  else
 	  {
@@ -73,22 +61,22 @@ public class Queue {
 	  display(rear);
 	}
 	
-
-	public static void dequeue ()
+	//Function To Delete Element In Queue
+	public  void dequeue ()
 	{
 	  if (front == -1)
 	  {
-		  System.out.println("Queue Underflow");
+		  System.out.println("Underflow\n");
 	  }
 	  else
 	  {
 	    item = Ar[front];
-	    System.out.printf("\nThe element deleted is %d\n", item);
-	    if (front == rear){
-	  
+	    System.out.printf("The deleted element in the queue: %d\n", item);
+	    if (front == rear)
+	    {
 	      front = -1;
 	      rear = -1;
-	      System.out.println("Error! Queue is empty");
+	      System.out.printf("Queue is empty\n");
 	    }
 	    else
 	    {
@@ -96,17 +84,15 @@ public class Queue {
 	      display(rear);
 	    }
 	  }
-}
+	}
 	
-	
-	
-	public static void display(int rear)
+	public void display(int rear)
 	{
 	  int i;
-	  System.out.printf("\nThe current Queue is\n");
+	  System.out.printf("\nThe elements in the queue are: \n");
 	  for (i = front; i <= rear; i++)
 	  {
-		  System.out.printf("\n%d ", Ar[i]);
+		  System.out.printf("%d ", Ar[i]);
 	  }
 	  System.out.printf("\n");
 	}
